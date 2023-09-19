189a365368607b04c60189371c5dff0783a62f22
melvinsjostrand <mesj04001@utb.vaxjo.se>
2023-09-19 16:39:40 +0200

    spelare

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@
         let playerNames = [];
         let scoreInputs = [];
         let playerInput = [];
+        let clear;
 
         // JavaScript function to update the bracket with player names
         function init() 
@@ -10,32 +11,34 @@
             scoreInputs = document.querySelectorAll('input[type="number"]');
             playerInput = document.querySelectorAll('input[type="text"]');
             updateBracket();
-            console.log(playerInput);
-            let clear = document.getElementById("btn1");
+            //console.log(playerInput);
+            clear = document.getElementById("btn1");
             clear.addEventListener("click" , event=>{
                 resetTournament();
             })
             let playername = document.getElementsByClassName("name");
             for(Element of playername){
-                console.log(Element);
+                //console.log(Element);
                 Element.addEventListener("input" , event=>{
                     updateBracket();
                 })
             }
-           // let i = 0;
-            /*scoreInputs.forEach((input) => {
-                input.addEventListener("change", function () {
-                    //console.log("input " + input.name.substring(5,6) + " " + input.name.substring(12,13));
-                    updateScore(i);
-                    i++;
-                });
-            });*/
-
-            for(let i = 1;i < scoreInputs.length;i+=2){
+            
+            let pos = 8;
+            for(let i = 0;i < scoreInputs.length;i++){
                 scoreInputs[i].addEventListener("change", function () {
                     //console.log("input " + input.name.substring(5,6) + " " + input.name.substring(12,13));
-                    updateScore(i);
-                    i++;
+                    //console.log(scoreInputs[i]);
+                    updateScore(i,pos);
+                    if(i%2===0){
+                        //console.log("hej " + i);
+                        i++;
+                    }else{
+                        //console.log("hej " + i);
+                        i++;
+                        pos++;  
+                    }                    
+                    //console.log("index: " + i + " pos: " + pos);
                 });
             }
         } // End init
@@ -59,66 +62,54 @@
         
         
 
-       /* document.addEventListener("DOMContentLoaded", function () {
-            const scoreInputs = document.querySelectorAll('input[type="number"]');
-            scoreInputs.forEach((input) => {
-                input.addEventListener("input", function () {
-                    updateScore(this);
-                });
-            });*/
-
         // JavaScript function to update the winner of each round based on scores
-        function updateScore(index) {
-            console.log("index " + index);
-            const scoreInputs = document.querySelectorAll('input[type="number"]');
-            const score1 = parseFloat(scoreInputs[index-1].value);
-            const score2 = parseFloat(scoreInputs[index].value);
+        function updateScore(index, position) {
+            let score1;
+            let score2;
+           
+           // const scoreInputs = document.querySelectorAll('input[type="number"]');
+            if(index%2===0){
+                //console.log("hej! " +index);
+                score1 = parseFloat(scoreInputs[index].value);
+                score2 = parseFloat(scoreInputs[index+1].value);
+                //console.log("Score 1 " + score1);
+            }else{
+                //console.log("hej2! " +index);
+                score1 = parseFloat(scoreInputs[index-1].value);
+                score2 = parseFloat(scoreInputs[index].value);
+            }            
+            
 
-            console.log("Score 1 " + score1);
-            console.log("score 2 " + score2);
+
+            //console.log("score 2 " + score2);
         
             if (!isNaN(score1) && !isNaN(score2)) {
-                console.log("Vinnare");
-                
+                //console.log("Vinnare");
+                //console.log("index " + index + " position: " + position);
                 //const winnerInput = document.querySelector('input[type="text"]');
                 if (score1 > score2) {
-                    console.log("Player 1");
-                    console.log("Spelare " + playerNames[index-1]);
-                    playerInput[index+7].value = playerNames[index-1];
+                    //console.log("Player 1");
+                    console.log("Player " + index + playerNames[index-1]);
+                    playerInput[position].value = playerNames[index-1];
+                    playerNames[position] = playerNames[index-1];
+                    console.log(playerNames);
                     //winnerInput.value = playerNames[gameContainer.dataset.player1 - 1];
                 } else if (score2 > score1) {
-                    console.log("Player 2");
-                    console.log("Spelare " + playerNames[index]);
-                    playerInput[index+7].value = playerNames[index];
+                    //console.log("Player 2 " + position);
+                    console.log("Player " + playerNames[index]);
+                    playerInput[position].value = playerNames[index];
+                    playerNames[position] = playerNames[index];
+                    console.log(playerNames);
+                    //console.log("Ruta: " + playerInput[position].value);
                     //winnerInput.value = playerNames[gameContainer.dataset.player2 - 1];
                 } else {
+                    playerInput[position].value = playerNames[index];
                     //winnerInput.value = "Tie";
                 }
         
-                // Update Semifinalist 1 input when appropriate
-                /*if (gameContainer.classList.contains('round-1')) {
-                    const round2Game = gameContainer.nextElementSibling.querySelector('.game-top input[type="text"]');
-                    if (round2Game) {
-                        round2Game.value = winnerInput.value;
-        
-                        // Move the winner to the semifinal match
-                        const semifinalContainer = round2Game.closest('.round');
-                        const semifinalInputs = semifinalContainer.querySelectorAll('input[type="text"]');
-                        semifinalInputs.forEach((input) => {
-                            if (input.value === "Semifinalist 1") {
-                                input.value = winnerInput.value;
-                            }
-                        });
-                    }
-                }*/
+                
             }
         }
- 
-        
-            // Initialize the bracket when the page loads
-          //  updateBracket();
-        //});
-        
         
 
         // JavaScript function to reset the tournament
