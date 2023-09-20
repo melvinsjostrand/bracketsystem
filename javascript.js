8d89bad9e2942f051954da8c3956ef4bec7b52ef
melvinsjostrand <mesj04001@utb.vaxjo.se>
2023-09-20 15:59:29 +0200

    feltestning

--- javascript.js
+++ javascript.js
@@ -11,34 +11,34 @@
             scoreInputs = document.querySelectorAll('input[type="number"]');
             playerInput = document.querySelectorAll('input[type="text"]');
             updateBracket();
-            //console.log(playerInput);
+            console.log(playerInput);
             clear = document.getElementById("btn1");
             clear.addEventListener("click" , event=>{
                 resetTournament();
             })
             let playername = document.getElementsByClassName("name");
             for(Element of playername){
-                //console.log(Element);
+                console.log(Element);
                 Element.addEventListener("input" , event=>{
                     updateBracket();
                 })
             }
             
             let pos = 8;
+            console.log("pos " + pos);
             for(let i = 0;i < scoreInputs.length;i++){
                 scoreInputs[i].addEventListener("change", function () {
-                    //console.log("input " + input.name.substring(5,6) + " " + input.name.substring(12,13));
-                    //console.log(scoreInputs[i]);
+                    console.log(scoreInputs[i]);
                     updateScore(i,pos);
                     if(i%2===0){
-                        //console.log("hej " + i);
+                        console.log("hej " + i);
                         i++;
                     }else{
-                        //console.log("hej " + i);
+                        console.log("hej " + i);
                         i++;
                         pos++;  
                     }                    
-                    //console.log("index: " + i + " pos: " + pos);
+
                 });
             }
         } // End init
@@ -64,44 +64,47 @@
 
         // JavaScript function to update the winner of each round based on scores
         function updateScore(index, position) {
+            console.log("position i update " + position);
+            console.log("index " + index);
             let score1;
             let score2;
            
-           // const scoreInputs = document.querySelectorAll('input[type="number"]');
+           
             if(index%2===0){
-                //console.log("hej! " +index);
-                score1 = parseFloat(scoreInputs[index].value);
+            
+                score1 = parseFloat(scoreInputs[index].value); //kan inte läsa av value andra rundan
                 score2 = parseFloat(scoreInputs[index+1].value);
-                //console.log("Score 1 " + score1);
+                console.log(score1);
+                console.log(score2);
+             
             }else{
-                //console.log("hej2! " +index);
-                score1 = parseFloat(scoreInputs[index-1].value);
+         
+                score1 = parseFloat(scoreInputs[index-1].value); //kan inte läsa av value andra rundan
                 score2 = parseFloat(scoreInputs[index].value);
+                console.log(score1);
+                console.log(score2);
             }            
             
 
 
-            //console.log("score 2 " + score2);
+
         
             if (!isNaN(score1) && !isNaN(score2)) {
-                //console.log("Vinnare");
-                //console.log("index " + index + " position: " + position);
-                //const winnerInput = document.querySelector('input[type="text"]');
+
                 if (score1 > score2) {
                     //console.log("Player 1");
                     console.log("Player " + index + playerNames[index-1]);
                     playerInput[position].value = playerNames[index-1];
                     playerNames[position] = playerNames[index-1];
                     console.log(playerNames);
-                    //winnerInput.value = playerNames[gameContainer.dataset.player1 - 1];
+     
                 } else if (score2 > score1) {
-                    //console.log("Player 2 " + position);
+                  
                     console.log("Player " + playerNames[index]);
                     playerInput[position].value = playerNames[index];
                     playerNames[position] = playerNames[index];
                     console.log(playerNames);
-                    //console.log("Ruta: " + playerInput[position].value);
-                    //winnerInput.value = playerNames[gameContainer.dataset.player2 - 1];
+        
                 } else {
                       //När semifinalen blir lika = undefined.
                       //hur ska vi lägga upp det ifall det blir lika en match. just nu blir det pos++ vi måste göra så pos är samma som den va under den matchen
@@ -114,7 +117,7 @@
         
 
         // JavaScript function to reset the tournament
-        function resetTournament() {
+        function resetTournament(i,pos) {
             // Clear player names
             for (let i = 1; i <= 8; i++) {
                 document.getElementById(`player${i}`).value = '';
@@ -133,9 +136,15 @@
 
 
             scoreInputs = [];
-            console.log(scoreInputs);
+            console.log("score i clear knapp " + scoreInputs);
             playerInput = [];
-            console.log(playerInput);
+            console.log("player i clear knapp " + playerInput);
+            pos = 8;
+            console.log("pos i clear knapp" + position);
+            i = 0;
+            console.log("i clear " + index);
         }
 
 
+//position = 15 efter clear
+//index = 1 men ska vara 0
